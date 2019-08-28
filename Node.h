50b4a4424c954b12bf3d3437a6dd2f93d7537ee8
Alejandro Goiochea <alejandrogoicochea@gmail.com>
2019-08-28 17:42:03 -0500

    Refactor(Changed Node.h): Changed Node.h to template.

--- Node.h
+++ Node.h
@@ -3,9 +3,23 @@
 
 template <typename T>
 class Node {
-protected:
-  T value;
-  Node *next;
+
+    protected:
+
+        T value;
+
+        Node<T> * next;
+
+    public:
+
+        Node(void){
+
+        }
+
+        ~Node(void){
+
+        }
+
 };
 
 #endif
