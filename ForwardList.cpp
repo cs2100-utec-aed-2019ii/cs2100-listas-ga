123362e2f7475f560e068d18aaeacddf3174dfe2
Alejandro Goiochea <alejandrogoicochea@gmail.com>
2019-08-29 16:11:49 -0500

    Refactor(Added methods): Added empty methods for the ForwardList class.

--- ForwardList.cpp
+++ ForwardList.cpp
@@ -1,2 +1,31 @@
 #include "Forwardlist.h"
 
+T& front(void){};
+
+T& back(void){};
+
+
+
+void push_back(const T& element){};
+
+void push_front(const T& element){};
+
+Node<T>* pop_back(void){};
+
+Node<T>* pop_front(void){};
+
+T& operator[] (const int&){};
+
+
+
+bool empty(void){};
+
+unsigned int size(void){};
+
+void clear(void){};
+
+ForwardList& sort(void){};
+
+ForwardList& reverse(void){};
+
+std::ostream& operator<< (const ForwardList&, std::ostream&){};
--- ForwardList.h
+++ ForwardList.h
@@ -1,13 +1,54 @@
-#ifdef FORWARDLIST_H
+#ifndef FORWARDLIST_H
 #define FORWARDLIST_H
 #include "Node.h"
 
-template<typename T>
-class Forwardlist
-{
-	protected:
-		Node<T>* head
+template <typename T>
 
+class ForwardList {
+
+    protected:
+
+        Node<T> *head;
+
+    public:
+
+        ForwardList(void){
+
+        }
+
+        ~ForwardList(void){
+
+        }
+
+        T& front(void);
+
+        T& back(void);
+
+
+
+        void push_back(const T& element);
+
+        void push_front(const T& element);
+
+        Node<T>* pop_back(void);
+
+        Node<T>* pop_front(void);
+
+        T& operator[] (const int&);
+
+
+
+        bool empty(void);
+
+        unsigned int size(void);
+
+        void clear(void);
+
+        ForwardList& sort(void);
+
+        ForwardList& reverse(void);
+
+        std::ostream& operator<< (const ForwardList&, std::ostream&);
 
 };
 
