ade6233ae94da668556f3dbfff11c8a16d84cc84
Gonzalo <gonzalo.alfacaso@gmail.com>
2019-09-12 16:40:41 -0500

    Back methods debug

--- ForwardList.h
+++ ForwardList.h
@@ -8,173 +8,160 @@
 template <typename T>
 class ForwardList : public List<T>{
 
-template<typename _T>
-inline friend std::ostream& operator<< (std::ostream& out, ForwardList<_T>& list){
-    ForwardListNode<_T> *temp = list.head;
-            while(temp){
-                out<< temp->value;
-                temp = temp->next;
-            }
-    return out;
-}
+	template<typename _T>
+		inline friend std::ostream& operator<< (std::ostream& out, ForwardList<_T>& list){
+			ForwardListNode<_T> *temp = list.head;
+			while(temp){
+				out<< temp->value;
+				temp = temp->next;
+			}
+			return out;
+		}
 
-template<typename _T>
-inline friend ForwardList<_T>& operator<< (const _T& element, ForwardList<_T>& list){
-    list->push_back(element);
-}
+	template<typename _T>
+		inline friend ForwardList<_T>& operator<< (const _T& element, ForwardList<_T>& list){
+			list->push_back(element);
+		}
 
-template<typename _T>
-inline friend ForwardList<_T>& operator>> (ForwardListNode<_T>& out, ForwardList<_T>& list){
-    out = list->pop_back();
-}
+	template<typename _T>
+		inline friend ForwardList<_T>& operator>> (ForwardListNode<_T>& out, ForwardList<_T>& list){
+			out = list->pop_back();
+		}
+	protected:
+	ForwardListNode<T>* head;
+	ForwardListNode<T>* tail;
 
-    protected:
+	public:
+	ForwardList():head{nullptr},tail{nullptr}{};
 
-        ForwardListNode<T> *head;
+	~ForwardList(){
+		this->clear();
+	}
 
-        ForwardListNode<T> *tail;
+	T& back(void){
+		auto node = this->head;
+		while(node->next != nullptr){
+			node = node->next;
+		}
+		return node->value;
+	}
 
-    public:
+	void push_front(const T& element){
+		auto temp = new ForwardListNode<T>(element);
+		temp->next = head;
+		head = temp;
+	}
 
-        ForwardList():head{nullptr},tail{nullptr}{};
-
-        ~ForwardList(){
-            this->clear();
-        }
-
-        T& back(void){
-            ForwardListNode<T>* node = this->head;
-            while(node->next){
-                node = node->next;
-	        }
-	        return node->value;
-        };
-
-        void push_front(const T& element){
-            ForwardListNode<T> *temp = new ForwardListNode<T>(element);
-            temp->next = head;
-            head = temp;
-        };
-
-        ForwardListNode<T>* pop_front(void){
-            ForwardListNode<T>* temp = head;
-            head = head->next;
-            return temp;
-        };
+	Node<T>* pop_front(void){
+		auto temp = head;
+		head = head->next;
+		return temp;
+	}
 
 
-        unsigned int size(void){
-            ForwardListNode<T>* temp = head;
-            unsigned int size = 0;
+	unsigned int size(void){
+		auto temp = head;
+		unsigned int size = 0;
 
-            while(temp){
-                size++;
-                temp = temp->next;
-            }
-            return size;
-        };
+		while(temp){
+			size++;
+			temp = temp->next;
+		}
+		return size;
+	};
 
-        void push_back(const T& element){
-            ForwardListNode<T>* nodeElement = new ForwardListNode<T>(element);
-            ForwardListNode<T>* node = this->head;
-            while(node->next != nullptr){
-                node = node->next;
-            }
-            node->next = nodeElement;
-            nodeElement->next = nullptr;
-        }
+	void push_back(const T& element){
+		auto nodeElement = new ForwardListNode<T>(element);
+		auto node = this->head;
+		while(node->next != nullptr){
+			node = node->next;
+		}
+		node->next = nodeElement;
+		nodeElement->next = nullptr;
+	}
 
-        ForwardListNode<T>* pop_back(void){
-            ForwardListNode<T>* node = this->head;
-            while((node->next)->next != nullptr){
-                node = node->next;
-            }
-			ForwardListNode<T>* nodeR = node->next;	
-            node->next = nullptr;
-						return nodeR;
-        }
+	Node<T>* pop_back(void){
+		auto node = this->head;
+		auto temp = node;
+		while((node->next)->next != nullptr){
+			node = node->next;
+		}
+		temp = node->next;
+		node->next = nullptr;
+		return(temp);
+	}
 
-        void erase(ForwardListNode<T>* del){
-            ForwardListNode<T>* temp = head;
-            while(temp->next != del){
-                temp = temp->next;
-            }
-            temp->next = (temp->next)->next;
-        }
+	void erase(ForwardListNode<T>* del){
+		auto temp = head;
+		while(temp->next != del){
+			temp = temp->next;
+		}
+		temp->next = (temp->next)->next;
+	}
 
-        void insert(ForwardListNode<T>* _node, const T& val){ // Inserta un elemento en la lista en base a un puntero
-            ForwardListNode<T>* temp = head;
-            ForwardListNode<T>* newNode = new ForwardListNode<T>(val);
-            if(_node == head){
-                newNode->next = _node;
-                head = newNode;
-            }
-            else{
-                while(temp->next != _node){
-                    temp = temp->next;
-                }
+	void insert(ForwardListNode<T>* _node, const T& val){ // Inserta un elemento en la lista en base a un puntero
+		auto temp = head;
+		auto newNode = new ForwardListNode<T>(val);
+		if(_node == head){
+			newNode->next = _node;
+			head = newNode;
+		}
+		else{
+			while(temp->next != _node){
+				temp = temp->next;
+			}
 
-                newNode->next = temp->next;
+			newNode->next = temp->next;
 
-                temp->next = newNode;
+			temp->next = newNode;
 
-            }
-        }
+		}
+	}
 
-        void drop(const T& val){ // Elimina todos los elementos de la lista que tienen el valor igual al parametro
-          ForwardListNode<T>* node = this->head;
-          ForwardListNode<T>* temp = node;
-					while(node->next != nullptr){
-								if(node->value == val && node != head){
-									temp->next = node->next;
-									delete node;
-								}
-									
-								*temp = &node;
-                &node = node.next;
-								if(temp == head){
-									~temp;
-									temp = &node;
-								}								
-          }
+	void drop(const T& val){ // Elimina todos los elementos de la lista que tienen el valor igual al parametro
+		auto node = this->head;
+		auto temp = node->next;
+		while(node->next != nullptr){
+			delete node;
+			node = temp;
+			temp = temp->next;
+		}
+	}
 
+	void sort(void){
+		auto temp = head;
+		T tempVal;
+		unsigned int _size = this->size();
+		for(unsigned int i = 0; i < _size - 1; i++){
+			for(unsigned int j = 0; j < _size - 1 - i; j++){
+				if(temp->value > (temp->next)->value){
+					tempVal = temp->value;
+					temp->value = (temp->next)->value;
+					(temp->next)->value = tempVal;
+				}
+				temp = temp->next;
+			}
+		}
 
-        }
+	}
 
-        void sort(void){
-            ForwardListNode<T>* temp = head;
-            T tempVal;
-            unsigned int _size = this->size();
-            for(unsigned int i = 0; i < _size - 1; i++){
-                for(unsigned int j = 0; j < _size - 1 - i; j++){
-                    if(temp->value > (temp->next)->value){
-                        tempVal = temp->value;
-                        temp->value = (temp->next)->value;
-                        (temp->next)->value = tempVal;
-                    }
-                    temp = temp->next;
-                }
-            }
+	void reverse(void){
+		auto node = this->head;
+		std::vector<T*> pointers;
+		while(node.next != nullptr){
+			pointers.push_back(node);
+			node = node.next;
+		} 
 
-        }
+		this->head = pointers.back();
+		node = this->head;
 
-        void reverse(void){
-					ForwardListNode<T> node = this->*head;
-					std::vector<T> pointers;
-					while(node.next != nullptr){
-						pointers.push_back(*node);
-						*node = node.next;
-          } 
-					
-					this->*head = pointers.back();
-					node = this->*head;
+		for(auto i=pointers.size()-1; i>=0; i--){
+			node.next = pointers[i];
+			node = node->next;
+		}
 
-					for(auto i=pointers.size()-1; i>=0; i--){
-						node.next = pointers[i];
-						&node = node.next;
-					}
-
-        }
+	}
 
 };
 
